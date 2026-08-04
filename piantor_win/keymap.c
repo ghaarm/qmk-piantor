5587ef7ce67bc191a87a25b7d38d4290319bfe55
ghaarm <ghaarm@tutanota.com>
2026-08-04 14:18:22 +0200

    mistake ctl alt del

--- piantor_win/keymap.c
+++ piantor_win/keymap.c
@@ -553,7 +553,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      QK_REP,                                            KC_NO,      KC_NO,      KC_NO,      KC_NO,     KC_NO,      KC_NO,
         _______,    KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,                                            KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_SCLN,    QK_BOOT,
         KC_NO,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,                                           KC_NO,      KC_MPRV,    KC_VOLD,    KC_VOLU,    KC_MNXT,    KC_MPLY,
-        _______,    _______,    _______,                    _______,  LCTL(LALT(KC_DEL)),    _______
+        _______,    LCTL(LALT(KC_DEL)),    _______,                    _______,  _______,    _______
 
 
     )
